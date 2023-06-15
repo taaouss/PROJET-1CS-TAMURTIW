2a40a36ab12f4163c7846ed6dafec6d9579a7d96
ManelGoudjil <km_goudjil@esi.dz>
2023-06-15 20:11:25 +0100

    feedback

--- FRONT/src/App.js
+++ FRONT/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router ,Route,Routes} from 'react-router-dom';
 import './App.css';
-import PayementReussi from './Composants/PayementReussi/PayementReussi';
+/*import PayementReussi from './Composants/PayementReussi/PayementReussi';
 import PagePayement from './Composants/PagePayement/pagePayement';
 import AdminStatic from './Composants/AdminStatistic/AdminStatic';
 import AppAdmin from './Composants/Admin/AppAdmin';
@@ -13,23 +13,24 @@ import FormulaireHoraire from './Composants/AdminGestion/GestionInformations/For
 import FormulaireDescription from './Composants/AdminGestion/GestionInformations/Formulaires/FormulaireDescription';
 import Formulaire from './Composants/AdminGestion/GestionInformations/Formulaires/Formulaire';
 import Informations from './Composants/AdminGestion/GestionInformations/Informations';
-import FormulaireAjouterEvenement from './Composants/AdminGestion/GestionEvenements/FormulaireAjouterEvenement';
+import FormulaireAjouterEvenement from './Composants/AdminGestion/GestionEvenements/FormulaireAjouterEvenement';*/
+import Review from './Composants/Feedback/Review';
+
 function App() {
   return(
   
  
- <>
+/* <>
 
 
  <Router> 
  <Routes>
- <Route index element={<AppAdmin></AppAdmin>}/>
- <Route path="Formulaire"  element={<Formulaire/>}/>
- <Route path="MiseAjourEvenements"  element={  <FormulaireAjouterEvenement></FormulaireAjouterEvenement>}/>
+
  </Routes>
  </Router> 
- </> 
 
+ </> */
+ <Review />
   )
  
 }
