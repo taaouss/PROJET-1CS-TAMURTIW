a0bcd44fcda0a64695029d1e4392aaab1f36d2ba
taous <kt_habbak@esi.dz>
2023-06-17 22:20:19 +0200

    modifs

--- FRONT/src/Composants/ResponsableCentral/AppRespo.js
+++ FRONT/src/Composants/ResponsableCentral/AppRespo.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import {React ,useState}from 'react'
 import ListeLieux from '../GestionLieux/ListeLieux'
+import ReactMapGL from 'react-map-gl'
 
 function AppRespo() {
+  const [viewport, setViewport] = useState({
+    width: '100vw',
+    height:'100vh',
+    latitude: 37.7577,
+    longitude: -122.4376,
+  });
+  
   return (
     <div>
       <div className='bar'/>
       <div className='containerA'>
-          <div className='Map'></div>
+          <div className='Map'>
+        
+          </div>
           <div className='InfoContainer'>
              <ListeLieux/>
           </div>
